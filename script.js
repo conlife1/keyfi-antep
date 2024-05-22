fbb1da43563686509b63ca9edc84c6e5daf88ed1
conlife1 <167481387+conlife1@users.noreply.github.com>
2024-05-22 13:49:03 +0300

    Update script.js
    eklemeler

--- script.js
+++ script.js
@@ -40,3 +40,72 @@ function changeLanguage(lang) {
             break;
     }
 }
+// Dil seçimini takip etmek için bir olay dinleyicisi ekleme
+function changeLanguage() {
+    // Seçilen dilin değerini al
+    var selectedLanguage = document.getElementById("languageSelect").value;
+
+    // Tüm sayfa içeriğini çevir
+    translatePage(selectedLanguage);
+}
+
+// Sayfa içeriğini çevirmek için bir fonksiyon ekleme
+function translatePage(language) {
+    // Tüm çevrilebilir öğeleri al
+    var translatableElements = document.querySelectorAll("[data-translate]");
+
+    // Her çevrilebilir öğe için işlem yap
+    translatableElements.forEach(function(element) {
+        // Öğenin veri-translate özniteliğinden çevrilecek metni al
+        var key = element.getAttribute("data-translate");
+        
+        // Çeviri verilerini içeren dil dosyasını al
+        var languageData;
+        switch (language) {
+            case "tr":
+                languageData = languageTR;
+                break;
+            case "en":
+                languageData = languageEN;
+                break;
+            case "fr":
+                languageData = languageFR;
+                break;
+            case "es":
+                languageData = languageES;
+                break;
+            case "de":
+                languageData = languageDE;
+                break;
+            case "it":
+                languageData = languageIT;
+                break;
+            case "ru":
+                languageData = languageRU;
+                break;
+            case "ar":
+                languageData = languageAR;
+                break;
+            case "ja":
+                languageData = languageJA;
+                break;
+            case "ko":
+                languageData = languageKO;
+                break;
+            case "zh":
+                languageData = languageZH;
+                break;
+            default:
+                languageData = languageEN;
+        }
+        
+        // Çeviri verilerinden ilgili metni al ve öğenin içeriğini güncelle
+        if (languageData[key]) {
+            element.textContent = languageData[key];
+        }
+    });
+}
+
+// Sayfa yüklendiğinde varsayılan olarak İngilizce çeviriyi uygula
+translatePage("en");
+
