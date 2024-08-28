47f1bd78babc7839512b5919a9ef3374e25e494b
conlife1 <167481387+conlife1@users.noreply.github.com>
2024-08-28 15:16:19 +0300

    Update styles24.js
    das

--- styles24.js
+++ styles24.js
@@ -12,3 +12,32 @@ function closeModal() {
     var modal = document.getElementById("modal");
     modal.style.display = "none";
 }
+// Modal elementleri
+var modal = document.getElementById("photo-modal");
+var img = document.getElementById("modal-img");
+var description = document.getElementById("modal-description");
+var span = document.getElementsByClassName("close")[0];
+
+// Fotoğrafa tıklandığında modal'ı aç
+document.querySelectorAll('.photo-link').forEach(item => {
+    item.addEventListener('click', event => {
+        event.preventDefault();
+        var photoSrc = item.getAttribute('data-photo');
+        var photoDesc = item.getAttribute('data-description');
+        img.src = photoSrc;
+        description.textContent = photoDesc;
+        modal.style.display = "block";
+    });
+});
+
+// Kapatma butonuna tıklandığında modal'ı kapat
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Modal dışına tıklandığında modal'ı kapat
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
